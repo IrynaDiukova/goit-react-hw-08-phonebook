0d5da60cc6d2827737454ae92b43e62e457754dc
Iryna Diukova <diukovairyna@ukr.net>
2023-03-17 19:03:51 +0200

    исправил

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,17 +1,41 @@
+import { Box, IconButton, ListItem } from '@mui/material';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/contacts/contactsOperations';
-import css from './ContactItem.module.css'
+import PersonIcon from '@mui/icons-material/Person';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export const ContactItem = ({ id, name, number }) => {
     const dispatch = useDispatch();
 
     return (
-       
-        <li> 
-            <span className={css.contactText}> {name}:  {number} </span>
-            <button className={css.contactBtn} onClick={() => dispatch(deleteContact(id))}> Delete </button>
-        </li>)};
+      <ListItem
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+        divider
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+          component="span"
+        >
+          <PersonIcon />
+          {name}: {number}
+        </Box>
+        <IconButton
+          size="small"
+          aria-label="delete"
+          onClick={() => dispatch(deleteContact(id))}
+        >
+          <DeleteIcon />
+        </IconButton>
+      </ListItem>
+    );};
 
 ContactItem.propTypes = {
     id: PropTypes.string.isRequired,
--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -11,32 +11,35 @@ export const authSlice = createSlice({
     },
     extraReducers: builder => {
         builder
-          .addCase(register.pending, (state, action) => state)
+          // .addCase(register.pending, (state, action) => state)
           .addCase(register.fulfilled, (state, action) => {
             state.user = action.payload.user;
             state.token = action.payload.token;
             state.isLoggedIn = true;
           })
-          .addCase(register.register, (state, action) => state)
+          // .addCase(register.register, (state, action) => state)
           .addCase(logIn.fulfilled, (state, action) => {
             state.user = action.payload.user;
             state.token = action.payload.token;
             state.isLoggedIn = true;
           })
-          .addCase(logOut.fulfilled, (state, action) => {
+          .addCase(logOut.fulfilled, state => {
             state.user = { name: null, email: null };
             state.token = null;
             state.isLoggedIn = false;
             state.isRefreshing = false;
           })
-          .addCase(refreshUser.pending, (state, action) => {
+          .addCase(refreshUser.pending, state => {
             state.isRefreshing = true;
           })
           .addCase(refreshUser.fulfilled, (state, action) => {
             state.user = action.payload;
             state.isLoggedIn = true;
             state.isRefreshing = false;
-          });
+          })
+          .addCase(refreshUser.rejected, state => {
+          state.isRefreshing = false;
+      });
     },
 });
 
